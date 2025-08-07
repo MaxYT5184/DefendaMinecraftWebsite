e96ade5c5183e883d709985d3acc7583edceb179
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-08-07 15:57:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function copyIP() {
-    const ipText = document.querySelector(".ip").textContent;
-    navigator.clipboard.writeText(ipText).then(() => {
-        alert("Server IP copied to clipboard: " + ipText);
-    });
+  const ipText = document.querySelector(".ip").textContent;
+  navigator.clipboard.writeText(ipText).then(() => {
+    alert(`Server IP copied: ${ipText}`);
+  });
 }
